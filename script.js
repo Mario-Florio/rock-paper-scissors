b5744be134196fe878eb7f25c805d11f3602483d
ketherball <121590283+ketherball@users.noreply.github.com>
2023-01-15 13:16:01 -0500

    Created function 'playRound' which plugs in the variables 'playerChoice' and 'computerChoice' and returns an alert with results depending on variable values

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+//Function below randomly selcts 'Rock', 'Paper', or 'Scissors'
 function getComputerChoice() {
     let rock = 'Rock'
     let paper = 'Paper'
@@ -13,8 +13,44 @@ function getComputerChoice() {
     }
 }
 
+//Result of 'getComputerChoice' gets assigned to variable 'computerChoice'
 let computerChoice = getComputerChoice()
 
 console.log(computerChoice)
 
-//function playRound(playerSelection, CPU)
+//Function below prompts user for choice of 'Rock', 'Paper', or 'Scissors' and returns results in lower case for case insensitivity
+function getPlayerChoice() {
+    let playerChoice = prompt("Rock, Paper, or Scissors?")
+    return playerChoice.toLowerCase()
+}
+//Result of 'getPlayerChoice' gets assigned to variable 'playerChoice'
+let playerChoice = getPlayerChoice()
+
+console.log(playerChoice)
+
+//Function below plugs in 'playerChoice' and 'computerChoice' and alerts user of the choices and results
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice === 'rock' && computerChoice === 'Scissors') {
+        alert('Computer: Scissors \nYou: Rock \nRock beats scissors. You win!')
+    } else if (playerChoice === 'rock' && computerChoice === 'Paper') {
+        alert('Computer: Paper \nYou: Rock \nPaper beats rock. You lose.')
+    } else if (playerChoice === 'rock' && computerChoice === 'Rock') {
+        alert('Computer: Rock \nYou: Rock \nIts a draw.')
+    } else if (playerChoice === 'paper' && computerChoice === 'Rock') {
+        alert('Computer: Rock \nYou: Paper \nPaper beats rock. You Win!')
+    } else if (playerChoice === 'paper' && computerChoice === 'Scissors') {
+        alert('Computer: Scissors \nYou: Paper \nScissors beats paper. You lose.')
+    } else if (playerChoice === 'paper' && computerChoice === 'Paper') {
+        alert('Computer: Paper \nYou: Paper \nIts a draw.')
+    } else if (playerChoice === 'scissors' && computerChoice === 'Paper') {
+        alert('Computer: Paper \nYou: Scissors \nScissors beats paper. You win!')
+    } else if (playerChoice === 'scissors' && computerChoice === 'Rock') {
+        alert('Computer: Rock \nYou: Scissors \nRock beats scissors. You lose.')
+    } else if (playerChoice === 'scissors' && computerChoice === 'Scissors') {
+        alert('Computer: Scissors \nYou: Scissors \nIts a draw.')
+    } else {
+        alert(`Computer: ${computerChoice} \nYou: \nNo contest. You need to make choice`)
+    }
+}
+
+playRound(playerChoice, computerChoice)
